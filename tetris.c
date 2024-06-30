afe87d9e11f34000b346436b15c1abb953abec91
otilevad <arthurdavelvb.com>
2024-06-29 23:23:37 -0300

    jogo 1.1 (espaço)

--- tetris.c
+++ tetris.c
@@ -5,14 +5,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libtetris.h"
+#define COLOR_ORANGE 8
 int ch=0;
 int game=1;
 void mostraTabuleiro(int fonteMatriz[TAMANHO][TAMANHO]){
+    int cor;
+    init_color(COLOR_ORANGE, 1000, 350, 0);
+    init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(3, COLOR_CYAN, COLOR_BLACK);
+    init_pair(4, COLOR_ORANGE, COLOR_BLACK);
+    init_pair(5, COLOR_BLUE, COLOR_BLACK);
+    init_pair(6, COLOR_RED, COLOR_BLACK);
+    init_pair(7, COLOR_GREEN, COLOR_BLACK);
+
     for(int l = 0; l < TAMANHO; l++){
         mvaddstr(l+1, 0,"║");
         for(int c = 0; c < TAMANHO; c++){
             mvaddstr(0, c+1,"═");
             int fonteN=fonteMatriz[l][c];
+            if(fonteN<0){
+                cor=(fonteN*-1)-1;
+            }
+            else{
+                cor=fonteN -1;
+            }
+            attron(COLOR_PAIR(cor));
             switch(fonteN){
                 case 0:
                 mvaddstr(l+1, c+1, " ");
@@ -29,12 +47,12 @@ void mostraTabuleiro(int fonteMatriz[TAMANHO][TAMANHO]){
                 }
                 break;
             }
+            attroff(COLOR_PAIR(cor));
             mvaddstr(TAMANHO+1, c+1,"═");
         }
         printw("\n");
         mvaddstr(l+1, TAMANHO+1,"║");
     }
-
     mvaddstr(0, 0,"╔");
     mvaddstr(0, TAMANHO+1,"╗");
     mvaddstr(TAMANHO+1, TAMANHO+1,"╝");
@@ -42,99 +60,27 @@ void mostraTabuleiro(int fonteMatriz[TAMANHO][TAMANHO]){
     mvaddstr(TAMANHO+2, 0,""); //Reposiciona o ponteiro
     refresh();
 }
-void teste(peca pecas[7], int pecaAtual){
-    for(int l = 0; l < 4; l++){
-        for(int c = 0; c < 8; c++){
-            int fonteN=pecas[pecaAtual].grid1[l][c];
-            switch(fonteN){
-                case 0:
-                printw("▒");
-                break;
-                default:
-                printw("█");
-                break;
-            }
-        }
-        printw("\n");
-    }
-    printw("\n\n");
-    for(int l = 0; l < 4; l++){
-        for(int c = 0; c < 8; c++){
-            int fonteN=pecas[pecaAtual].grid2[l][c];
-            switch(fonteN){
-                case 0:
-                printw("▒");
-                break;
-                default:
-                printw("█");
-                break;
-            }
-        }
-        printw("\n");
-    }
-    printw("\n\n");
-    for(int l = 0; l < 4; l++){
-        for(int c = 0; c < 8; c++){
-            int fonteN=pecas[pecaAtual].grid3[l][c];
-            switch(fonteN){
-                case 0:
-                printw("▒");
-                break;
-                default:
-                printw("█");
-                break;
-            }
-        }
-        printw("\n");
-    }
-    printw("\n\n");
-    for(int l = 0; l < 4; l++){
-        for(int c = 0; c < 8; c++){
-            int fonteN=pecas[pecaAtual].grid4[l][c];
-            switch(fonteN){
-                case 0:
-                printw("▒");
-                break;
-                default:
-                printw("█");
-                break;
-            }
-        }
-        printw("\n");
-    }
-    refresh();
-}
-
 int main() {
     int matrizFonte[TAMANHO][TAMANHO] = {0};
     peca pecas[8];
     srand(time(NULL));
     setlocale(LC_ALL, "");
-    initscr();            // Inicia a janela ncurses
-    cbreak();             // Desabilita o buffer da linha
-    noecho();             // Não exibe a tecla pressionada
-    keypad(stdscr, TRUE); // Habilita a captura de teclas especiais
-    geraPecas(pecas);    // Gera e salva as peças no vetor
-    /*
-    int num=0
-    while (true) {
-        clear();
-        teste(pecas, num);
-        num++;
-        num%=7;
-        delay_output(2550);
-        refresh();        
-    }
-    */
-    int pecaSorteada = 2, colh = 0, colv =0, colg=0, nextOr=2, counter=0, delay=500;
+    initscr();              // Inicia a janela ncurses
+    start_color();         
+    cbreak();               // Desabilita o buffer da linha
+    noecho();               // Não exibe a tecla pressionada
+    keypad(stdscr, TRUE);   // Habilita a captura de teclas especiais
+    geraPecas(pecas);       // Gera e salva as peças no vetor
+    int pecaSorteada = 2, colh = 0, colv =0, colg=0, nextOr=2, counter=0, delayPad=500, delay=delayPad, delayFast=delayPad/20;
     int xpad = 6, ypad = -1;
     int x = xpad, y = ypad;
+    int xinicial, yinicial;
     while(game==1) {
         counter=0;
         imprimePeca(matrizFonte, pecas[pecaSorteada], y, x, 1);
         mostraTabuleiro(matrizFonte);
         while(1){
-            delay=500;
+            delay=delayPad;
             timeout(1);
             ch = getch();
             switch(ch){
@@ -165,7 +111,20 @@ int main() {
                     }
                 break;
                 case KEY_DOWN:
-                    delay=30;
+                    delay=delayFast;
+                break;
+                case ' ':
+                    xinicial=x;
+                    yinicial=y;
+                    colv=colisao(pecas[pecaSorteada], 1, 1, matrizFonte, y, x);
+                    while(colv==0){
+                        colv=colisao(pecas[pecaSorteada], 1, 1, matrizFonte, y, x);
+                        if(colv==0){
+                            y++;
+                        }
+                    }
+                    limpaPeca(matrizFonte, pecas[pecaSorteada], yinicial, xinicial);
+                    delay=0;
                 break;
             }
             imprimePeca(matrizFonte, pecas[pecaSorteada], y, x, 1);
